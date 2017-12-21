6f6705de1e1a9fd23ff36527cdab1fd16a763315
alexluty49 <33843453+alexluty49@users.noreply.github.com>
2017-12-21 08:50:34 +0600

    Add files via upload

---
+++ glasnie and soglasnie.py
@@ -0,0 +1,25 @@
+glasnie = ["а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е"]
+soglasnie = ["б", "в", "г", "д", "ж", "з", "й", "к", "л", "м", "н", "п", "р", "с", "т", "ф", "х", "ц", "ч", "ш", "щ"]
+
+
+
+print("Введите слово")
+slovo = input()
+c = tuple(slovo)
+
+for i in c:
+    print (i)
+    
+
+list = []
+d = list.append(i)
+print(list)
+
+if i in list[0] in glasnie:
+    print("Слово начинается с гласной буквы ")
+
+if i in list[0] in soglasnie:
+    print("Слово начинается с согласной буквы")
+
+
+
